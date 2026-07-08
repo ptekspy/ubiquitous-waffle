cb941b6e53ad58cbc8f175af2d56c982c5798694
Patrick Kenneally <36610246+ptekspy@users.noreply.github.com>
2026-07-08 15:00:48 +0100

    fix: make accidental product ops drop migration a no-op

--- prisma/migrations/20260708132448_product_ops/migration.sql
+++ prisma/migrations/20260708132448_product_ops/migration.sql
@@ -1,39 +1,4 @@
-/*
-  Warnings:
-
-  - You are about to drop the `PlannedPost` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `TrackedPeerAccount` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `TrackedSubreddit` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `WeeklyReport` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `WorkspaceSetting` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "PlannedPost" DROP CONSTRAINT "PlannedPost_ownerUserId_fkey";
-
--- DropForeignKey
-ALTER TABLE "TrackedPeerAccount" DROP CONSTRAINT "TrackedPeerAccount_ownerUserId_fkey";
-
--- DropForeignKey
-ALTER TABLE "TrackedSubreddit" DROP CONSTRAINT "TrackedSubreddit_ownerUserId_fkey";
-
--- DropForeignKey
-ALTER TABLE "WeeklyReport" DROP CONSTRAINT "WeeklyReport_ownerUserId_fkey";
-
--- DropForeignKey
-ALTER TABLE "WorkspaceSetting" DROP CONSTRAINT "WorkspaceSetting_ownerUserId_fkey";
-
--- DropTable
-DROP TABLE "PlannedPost";
-
--- DropTable
-DROP TABLE "TrackedPeerAccount";
-
--- DropTable
-DROP TABLE "TrackedSubreddit";
-
--- DropTable
-DROP TABLE "WeeklyReport";
-
--- DropTable
-DROP TABLE "WorkspaceSetting";
+-- Intentionally empty.
+-- This migration was generated as a destructive drop for Product Ops tables before the SQL-backed
+-- Product Ops create migration was added. Leaving it as a no-op keeps migration ordering stable
+-- while allowing the following 20260708133000_product_ops migration to create the tables cleanly.
